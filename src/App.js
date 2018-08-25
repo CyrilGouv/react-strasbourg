ab07b99c8401637dfaa63c165be5d29f2781dc55
Cyril Gouverneur <cyril@iMac-de-Cyril.local>
2018-08-25 16:54:29 +0200

    Create Header section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+
+import Home from './pages/Home';
+
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        APP TEST
-      </div>
+      <BrowserRouter>
+        <div className="App">
+
+
+          {/* Route */}
+          <Route exact path="/" component={ Home }/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
